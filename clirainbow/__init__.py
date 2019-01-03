29802465b4f21de25089cd72824f71b67e0fc616
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-01-03 18:24:14 -0500

    Bump version

--- clirainbow/__init__.py
+++ clirainbow/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 from .clirainbow import Colorizer, ColorBracketOpenedButNotClosed, MoreBracketsThanColors, MoreColorsThanBrackets
 from .colors import *
