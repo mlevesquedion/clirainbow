ec278ee1f00cd368504dbaf621937d67cf94484a
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-01-09 17:42:03 -0500

    Change main script to reflect current project name

--- clirainbow/__main__.py
+++ clirainbow/__main__.py
@@ -5,16 +5,17 @@ from clirainbow.colors import *
 def main():
     c = Colorizer()
     c.print(
-        'Welcome to <C><O><L><O><R><I><Z><E><R>',
+        'Welcome to <C><L><I><R><A><I><N><B><O><W>',
         BRIGHT_RED,
         BRIGHT_GREEN,
         BRIGHT_BLUE,
+        BRIGHT_YELLOW,
+        BRIGHT_CYAN,
+        BRIGHT_MAGENTA,
         BRIGHT_RED,
         BRIGHT_GREEN,
         BRIGHT_BLUE,
-        BRIGHT_RED,
-        BRIGHT_GREEN,
-        BRIGHT_BLUE
+        BRIGHT_YELLOW,
     )
 
 
